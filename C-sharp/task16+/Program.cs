1a3e4564463aab248ddb33cc6a35154d33d24712
4.4u <94313456+He4auHo@users.noreply.github.com>
2023-10-14 19:42:32 +0300

    решены задачи дз3, добавил 25,27,29

---
+++ C-sharp/task16+/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//string num=Convert.ToString(Console.ReadLine());
+
+string num=Console.ReadLine();
+if (num.Length==5){
+    if (num[0]==num[4]&&num[1]==num[3]){
+        //return true;
+        Console.WriteLine("палиндром");
+    }
+    else{
+        Console.WriteLine("не палиндром");
+    }
+}
+else{
+    Console.WriteLine("не пятизначное");
+    //return false;
+}
