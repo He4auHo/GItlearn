058b706b8f30add5e1acc6ca4146947e0c40ad9a
4.4u <94313456+He4auHo@users.noreply.github.com>
2023-10-22 22:55:25 +0300

    добавил решение задачи 27

--- C-sharp/task16+/Program.cs
+++ C-sharp/task16+/Program.cs
@@ -1,17 +1,43 @@
 ﻿
 //string num=Convert.ToString(Console.ReadLine());
-
-string num=Console.ReadLine();
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+      
+      string num=Convert.ToString(number);
 if (num.Length==5){
     if (num[0]==num[4]&&num[1]==num[3]){
-        //return true;
-        Console.WriteLine("палиндром");
+        return true;
+        //Console.WriteLine("палиндром");
     }
     else{
-        Console.WriteLine("не палиндром");
+        //Console.WriteLine("не палиндром");
+        return false;
     }
 }
 else{
-    Console.WriteLine("не пятизначное");
-    //return false;
+    Console.WriteLine("Число не пятизначное");
+    return false;
 }
+    }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 123456;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
+
+
+
--- C-sharp/task25+/Program.cs
+++ C-sharp/task25+/Program.cs
@@ -7,26 +7,5 @@ int B=Convert.ToInt16(Console.ReadLine());
 int res=1;
 for(int i=1;i<=B;i++){
 res=res*A;
-Console.WriteLine(A);
 }
  Console.WriteLine(res);   
-
-
-/*
-int a,b=0,dd=0;
-a=num;
-int i=2;
-int [] ar=new int[3];
-do{
-  a=num/10;
-  b=a*10;
-  dd=num-b;
-  num=a;
-  ar[i]=dd;
-  i=i-1;
-}
-while(a>1);
-
-} 
-task10();
-*/
