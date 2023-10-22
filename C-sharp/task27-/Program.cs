97587ec28658c77e11b48737e8bda4c7f45499fa
4.4u <94313456+He4auHo@users.noreply.github.com>
2023-10-22 22:53:37 +0300

    добавил решение задачи 27

--- C-sharp/task27-/Program.cs
+++ C-sharp/task27-/Program.cs
@@ -2,3 +2,17 @@
 452 -> 11
 82 -> 10
 9012 -> 12*/
+
+void summ_of_elements()
+{
+   string chislo=Console.ReadLine();
+   int res=0;
+    
+    for (int i=0; i<chislo.Length; i++)
+    {
+     res+= Convert.ToInt16(Convert.ToString(chislo[i]));
+    }
+Console.WriteLine(res);
+}
+
+summ_of_elements();
